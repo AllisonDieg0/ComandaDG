b466c689f1d0f909e4933b3ccca701f76243afa2
Allison Diego <80264145+AllisonDieg0@users.noreply.github.com>
2021-11-04 11:51:01 -0300

    Add files via upload

--- CrudPhP/MYSQLI/index.php
+++ CrudPhP/MYSQLI/index.php
@@ -1,18 +1,15 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Class Crud</title>
-</head>
-<body>
+<?php include("includes/header.php");?>
+<div class="Content">
 
 <?php
 
 include("Class/ClassConexao.php");
+include("Class/ClassCrud.php");
 
 ?>
+</div>
+
+<?php include("includes/footer.php");?>
+
+
     
-</body>
-</html>

912d34dd8fc07f79d43c8f0b5ad6c239969a6cdf
Allison Diego <80264145+AllisonDieg0@users.noreply.github.com>
2021-11-05 17:39:09 -0300

    Add files via upload

--- CrudPhP/PDO/cadastro.php
+++ CrudPhP/PDO/cadastro.php
@@ -10,7 +10,31 @@ include("Class/ClassCrud.php");
 ?>
 <div class="Resultado"></div>
 <div class="Formulario">
-    <h1 class="center"> Cadastro </h1>
+    <h1 class="center"> Cadastro De Pedidos</h1>
+
+    <div class="FormularioInput">
+        Cliente: <br>
+        <input type="text" id="cli"  name="nomeCli">
+
+    </div>
+
+    <div class="FormularioInput">
+        Pedido: <br>
+        <input type="text" id="ped"  name="pedido">
+        
+    </div>
+
+    <div class="FormularioInput">
+        Observação: <br>
+        <input type="text" id="obs"  name="obs">
+        
+    </div>
+
+    <div class="FormularioBtn FormularioInput100 center">
+        
+        <input type="submit", value="Cadastrar">
+        
+    </div>
 </div>
 
 </div>
